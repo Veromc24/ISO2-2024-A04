a5d68cb23fb75b7bcb5cc2890f942124a412e77c
dgms02 <davidguzmanms@gmail.com>
2024-12-11 22:52:30 +0100

    Component 18 FR.14.G Version 1.0.0

---
+++ ISO2_2024_A04_CLIENT/src/main/java/ISOFT2/ISO2_2024_A04_CLIENT/Domain/User.java
@@ -0,0 +1,15 @@
+package Domain;
+
+public class User {
+	private String _name;
+	private String _password;
+	private String _lastConnection;
+
+	public User() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void Authenticate() {
+		throw new UnsupportedOperationException();
+	}
+}
