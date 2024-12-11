8ebe180ee84117e7a3f6e4dec3cf8e330a9f96f0
dgms02 <davidguzmanms@gmail.com>
2024-12-11 12:59:28 +0100

    Component 4 FR.04.G.1.Version.1.0.0

---
+++ ISO2_2024_A04_CLIENT/src/main/java/ISOFT2/ISO2_2024_A04_CLIENT/Domain/Data_Project.java
@@ -0,0 +1,17 @@
+package Domain;
+
+public class Data_Project {
+	private Client[] _clients;
+	private String _iD;
+	private String _content;
+	public Control_data_projects _unnamed_Control_data_projects_;
+	public Client _unnamed_Client_;
+
+	public void DataProject(Client[] aClient) {
+		throw new UnsupportedOperationException();
+	}
+
+	public Client[] GetClient(Data_Project aData_Project) {
+		throw new UnsupportedOperationException();
+	}
+}
