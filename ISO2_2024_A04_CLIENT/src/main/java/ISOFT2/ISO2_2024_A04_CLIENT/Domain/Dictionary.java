1a6b5420ba2d5f047ea38af3b476c25bc76444e0
dgms02 <davidguzmanms@gmail.com>
2024-12-11 23:12:01 +0100

    error fixed

--- ISO2_2024_A04_CLIENT/src/main/java/ISOFT2/ISO2_2024_A04_CLIENT/Domain/Dictionary.java
+++
@@ -1,9 +0,0 @@
-package Domain;
-
-public class Dictionary {
-	private String _iD;
-	private String _name;
-	private String _type;
-	private Metadata[] _metadataList;
-	public Metadata _unnamed_Metadata_;
-}
--- ISO2_2024_A04_CLIENT/src/main/java/ISOFT2/ISO2_2024_A04_CLIENT/Domain/Glossary.java
+++
@@ -1,9 +0,0 @@
-package Domain;
-
-public class Glossary {
-	private String _iD;
-	private String _name;
-	private String _type;
-	private Metadata[] _metadataList;
-	public Metadata _unnamed_Metadata_;
-}
