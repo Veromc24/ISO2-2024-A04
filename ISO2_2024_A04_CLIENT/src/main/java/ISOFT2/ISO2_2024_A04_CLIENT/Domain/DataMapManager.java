3a0a7b1aa9f3eeaac59ad621f236475f01130c88
Carlos Ruiz García-Casarrubios <129770506+carlosrz022@users.noreply.github.com>
2024-12-11 12:48:31 +0100

    Component 7 FR.07.G Version 1.0.0

---
+++ ISO2_2024_A04_CLIENT/src/main/java/ISOFT2/ISO2_2024_A04_CLIENT/Domain/DataMapManager.java
@@ -0,0 +1,38 @@
+package Domain;
+
+public class DataMapManager {
+	private Set<String> _mapRules;
+	public DataMap _unnamed_DataMap_;
+
+	public void processProposedData() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void validateDataInput() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void DescribeData_using_Metadata(Metadata aMetadata) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void ClassifyData() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void SelectSources() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void ReferenceLineageForAnalysis() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void DescribeDataLyfecycle() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void ddToDataMap(Object aData) {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ ISO2_2024_A04_CLIENT/src/main/java/ISOFT2/ISO2_2024_A04_CLIENT/Persistance/PermissionDAO.java
@@ -0,0 +1,12 @@
+package Persistance;
+
+public class PermissionDAO {
+
+	public void savePermission() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void getPermission() {
+		throw new UnsupportedOperationException();
+	}
+}
