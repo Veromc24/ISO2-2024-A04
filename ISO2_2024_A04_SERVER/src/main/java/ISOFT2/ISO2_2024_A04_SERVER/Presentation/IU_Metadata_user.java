e6c7017d338644c0f3e6f78e151f945d0c31a8f1
veromc134 <veronica.martinez24@alu.uclm.es>
2024-12-11 16:49:31 +0100

    Component 13 FR.13.G V1.0.0

---
+++ ISO2_2024_A04_SERVER/src/main/java/ISOFT2/ISO2_2024_A04_SERVER/Presentation/IU_Metadata_user.java
@@ -0,0 +1,14 @@
+package Presentation;
+
+import Domain.Metadata;
+
+public class IU_Metadata_user {
+
+	public void startDescription() {
+		throw new UnsupportedOperationException();
+	}
+
+	public Metadata[] RequestMetadataList() {
+		throw new UnsupportedOperationException();
+	}
+}
