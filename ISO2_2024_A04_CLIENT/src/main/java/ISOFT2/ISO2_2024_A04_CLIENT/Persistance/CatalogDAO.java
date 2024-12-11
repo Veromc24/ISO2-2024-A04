fb2bd23023ca5519961fdbba50d90e464bb1c289
dgms02 <davidguzmanms@gmail.com>
2024-12-11 22:05:13 +0100

    Component 9 FR.0.9.G Version 1.0.0

---
+++ ISO2_2024_A04_CLIENT/src/main/java/ISOFT2/ISO2_2024_A04_CLIENT/Persistance/CatalogDAO.java
@@ -0,0 +1,14 @@
+package Persistance;
+
+import Domain.Catalog;
+
+public class CatalogDAO {
+
+	public vector<Object> GetGlossary() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void Newcatalog(Catalog aCatalog) {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ ISO2_2024_A04_CLIENT/src/main/java/ISOFT2/ISO2_2024_A04_CLIENT/Persistance/DictionaryDAO.java
@@ -0,0 +1,14 @@
+package Persistance;
+
+import Domain.Dictionary;
+
+public class DictionaryDAO {
+
+	public vector<Object> GetGlossary() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void NewDictionary(Dictionary aDictionary) {
+		throw new UnsupportedOperationException();
+	}
+}
