c927c7e0f18f147f04ebf23d4d592fcb1db18db6
veromc134 <veronica.martinez24@alu.uclm.es>
2024-12-11 16:55:28 +0100

    Component 14 FR.19.DO V1.0.0

---
+++ ISO2_2024_A04_CLIENT/src/main/java/ISOFT2/ISO2_2024_A04_CLIENT/Presentation/UI_DataOwner.java
@@ -0,0 +1,35 @@
+package Presentation;
+
+import Domain.RequestController;
+
+public class UI_DataOwner {
+	public RequestController _unnamed_RequestController_;
+
+	public void startProcess() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void CondigureProject() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void ReviewWorkflowResults() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void displayDataRequirements() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void CaptureDataInput() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void classifyData() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void SelectDataSources() {
+		throw new UnsupportedOperationException();
+	}
+}
