951a710149ed42752faed68c269d4f3d5c9883f8
oblivionwing26 <jorge.carrascosa1@alu.uclm.es>
2024-12-13 11:40:31 +0100

    Component 18 FR.14.G Version1.0.0

---
+++ ISO2_2024_A04_CLIENT/src/main/java/ISOFT2/ISO2_2024_A04_CLIENT/Domain/Authentication.java
@@ -0,0 +1,14 @@
+package Domain;
+
+public class Authentication {
+	private String _userName;
+	private String _passw;
+
+	public void AUthentication() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void VerifyAuthentication() {
+		throw new UnsupportedOperationException();
+	}
+}
