486c3a7477bb770d3dcb6099768e2412c65569c7
Carlos Ruiz García-Casarrubios <129770506+carlosrz022@users.noreply.github.com>
2024-12-13 10:30:43 +0100

    Component 5 FR.17.DO V1.0.0

---
+++ ISO2_2024_A04_CLIENT/src/main/java/ISOFT2/ISO2_2024_A04_CLIENT/Domain/DataClassifier.java
@@ -0,0 +1,16 @@
+package Domain;
+
+public class DataClassifier {
+	private int _classifierID;
+	private List<String> _rules;
+	public DataOwner _unnamed_DataOwner_;
+	public ProjectData _unnamed_ProjectData_;
+
+	public void classifyData(DataMap aData) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void validateClassification() {
+		throw new UnsupportedOperationException();
+	}
+}
