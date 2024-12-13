2a60e7d9051256d355f7745b8e9b03c8986dcec9
oblivionwing26 <jorge.carrascosa1@alu.uclm.es>
2024-12-13 12:13:03 +0100

    Component 18 FR.14.G Version 1.0.0

---
+++ ISO2_2024_A04_CLIENT/src/main/java/ISOFT2/ISO2_2024_A04_CLIENT/Persistance/UserDAO.java
@@ -0,0 +1,14 @@
+package Persistance;
+
+import Domain.User;
+
+public class UserDAO {
+
+	public Object<String> GetUser() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void SaveUser(User aUser) {
+		throw new UnsupportedOperationException();
+	}
+}
