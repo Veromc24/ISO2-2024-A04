02b1a46e5a99e8d2417b8545066d920b08d72547
Carlos Ruiz García-Casarrubios <129770506+carlosrz022@users.noreply.github.com>
2024-12-13 10:35:29 +0100

    Component 7 FR.25.SM V1.0.0

---
+++ ISO2_2024_A04_CLIENT/src/main/java/ISOFT2/ISO2_2024_A04_CLIENT/Presentation/UI_SecurityManager.java
@@ -0,0 +1,12 @@
+package Presentation;
+
+public class UI_SecurityManager {
+
+	public void initiatePermissionManagement() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void confirmPermissions() {
+		throw new UnsupportedOperationException();
+	}
+}
