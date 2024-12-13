b213461e969cf4f4ee56f6476f2884fe911fca0a
oblivionwing26 <jorge.carrascosa1@alu.uclm.es>
2024-12-13 12:54:38 +0100

    Component 1 FR.24.DS Version 1.2.0

---
+++ ISO2_2024_A04_CLIENT/src/main/java/ISOFT2/ISO2_2024_A04_CLIENT/Persistance/DataQualityReport.java
@@ -0,0 +1,16 @@
+package Persistance;
+
+public class DataQualityReport {
+
+	public void NewDataQualityReport() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void getDataQualityReport() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void saveDataQualityReport() {
+		throw new UnsupportedOperationException();
+	}
+}
